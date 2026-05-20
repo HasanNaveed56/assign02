03c9013fff041ef569de126e24cb6116ad7378ef
Hasan <pakigamer929@gmail.com>
2026-05-20 23:25:42 +0500

    my python assignment02

---
+++ assign02.py
@@ -0,0 +1,9 @@
+name = "Hasan"        # String
+age = 16              # Integer
+height = 5.7          # Float
+is_student = True     # Boolean
+
+print(name)
+print(age)
+print(height)
+print(is_student)
